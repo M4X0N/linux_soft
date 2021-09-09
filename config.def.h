95a6c36fb69dac314a12ed827415c598ffe3496b
m4x0n <m4x0n@arch24.localdomain>
2021-09-09 21:16:48 +0300

    Still having screenshot problem

--- config.def.h
+++ config.def.h
@@ -5,15 +5,15 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=11" };
-static const char dmenufont[]       = "monospace:size=11";
+static const char *fonts[]          = { "Liberation Mono:pixelsize=14:antialias=true:autohint=true" };
+static const char dmenufont[]       = "Liberation Mono:pixelsize=14:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 /*	GRUVBOX COLORS	*/
-static const char col_bg0[]			= "#1d2021";
+static const char col_bg0[]			= "#282828";
 static const char col_bg1[]			= "#3c3836";
 static const char col_bg2[]			= "#504945";
 static const char col_bg3[]			= "#665c54";
@@ -24,25 +24,32 @@ static const char col_fg1[]			= "#ebdbb2";
 static const char col_fg2[]			= "#d5c4a1";
 static const char col_fg3[]			= "#bdae93";
 static const char col_fg4[]			= "#a89984";
-
+// Pale
 static const char col_red[]			= "#cc241d";
 static const char col_green[]		= "#98971a";
 static const char col_yellow[]		= "#d79921";
 static const char col_blue[]		= "#458588";
 static const char col_purple[]		= "#b16286";
 static const char col_aqua[]		= "#689d6a";
+// Bright
+static const char col_0red[]		= "#fb4934";
+static const char col_0green[]		= "#b8bb26";
+static const char col_0yellow[]		= "#fabd2f";
+static const char col_0blue[]		= "#83a598";
+static const char col_0purple[]		= "#d3869b";
+static const char col_0aqua[]		= "#8ec07c";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_fg2,   col_bg0,   col_bg1 },
-	[SchemeSel]  = { col_fg0,   col_bg0,   col_bg4  },
+	[SchemeNorm] = { col_fg0,   col_bg0,   col_bg1 },
+	[SchemeSel]  = { col_0yellow,   col_bg0,   col_bg4  },
 };
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
-static const char ptagf[] = "[%s %s]";	/* format of a tag label */
-static const char etagf[] = "[%s]";	/* format of an empty tag */
+static const char ptagf[] = "%s:%s";	/* format of a tag label */
+static const char etagf[] = "%s";	/* format of an empty tag */
 static const int lcaselbl = 0;		/* 1 means make tag label lowercase */	
 
 static const Rule rules[] = {
@@ -84,15 +91,37 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg0, "-nf", col_fg4, "-sb", col_bg0, "-sf", col_fg0, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg0, "-nf", col_fg0, "-sb", col_bg0, "-sf", col_0yellow, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
-static const char *smenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg2, "-nf", col_fg0, "-sb", col_bg0, "-sf", col_fg0, "-x", 1420, "-y", 16,"-z", 500, "-l", 40, NULL };
+static const char *smenucmd[] = { "echo", "MENU", "|", "dmenu", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg0, "-nf", col_0blue, "-sb", col_0blue, "-sf", col_bg0, NULL };
+static const char *killcmd[] = { "xkill", NULL };
+
+static const char *passmenucmd[] = { "passmenu", NULL };
+
+
+static const char *sscmd[]  = { "sh", "~/.scripts/screenshot.sh", NULL };
+//static const char *sscmd[]  = { "maim -u | xclip -selection clipboard -t image/png -i", NULL };
+
+static const char *rangercmd[]  = { "st", "ranger", NULL };
+static const char *thunarcmd[]  = { "thunar", NULL };
+static const char *bravecmd[]	= { "brave", NULL };
+static const char *firefoxcmd[]	= { "firefox", NULL };
+
+static const char *topcmd[]  = { "st", "bpytop", NULL };
+
 
 static Key keys[] = {
 	/* modifier             key    function        argument */
+	{ MODKEY|ControlMask, 	119,   spawn,		   {.v = topcmd } }, // Ctrl Win Delete
+
 	{ MODKEY,               33,    spawn,          {.v = dmenucmd } }, // p
+	{ MODKEY,				135,   spawn,          {.v = dmenucmd } }, // options key
+	{ MODKEY|ShiftMask,		33,    spawn,          {.v = passmenucmd } }, // P
 	{ MODKEY|ShiftMask,     36,    spawn,          {.v = termcmd } }, // Return
+	{ MODKEY,               26,    spawn,          {.v = rangercmd } }, // e
+	{ MODKEY|ShiftMask,		26,    spawn,          {.v = thunarcmd } }, // e
+
 	{ MODKEY,               56,    togglebar,      {0} },             // b
 	{ MODKEY,               44,    focusstack,     {.i = +1 } },      // j
 	{ MODKEY,               45,    focusstack,     {.i = -1 } },      // k
@@ -127,6 +156,7 @@ static Key keys[] = {
 	{ MODKEY|ControlMask,	22,		quit,			{0} },				// Backspace
 	{ MODKEY|ShiftMask,		22,		quit,			{1} },				// Backspace
 
+	{ NULL,					107,	spawn,          {.v = sscmd } }, // PrntScrn
 	{ MODKEY|ShiftMask,		59,		cyclelayout,	{.i = -1 } },		// comma
 	{ MODKEY|ShiftMask,		60,		cyclelayout,    {.i = +1 } },		// period
 };
@@ -148,5 +178,6 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 
 	{ ClkStatusText,        0,              Button1,        spawn,          {.v = smenucmd } },
+	{ ClkStatusText,        0,              Button3,        spawn,          {.v = killcmd } },
 };
 
