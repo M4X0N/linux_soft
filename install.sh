418ab5f255b6c06b0be66147474483d102936104
Max Rogol <98maxon98@gmail.com>
2022-07-30 14:53:10 +0300

    added make/install script

--- install.sh
+++ install.sh
@@ -2,20 +2,40 @@
 # Additional wrapper to
 echo "Started"
 
-while getopts 'mu' OPTION; do
+echo "Cleaning"
+rm config.h &
+rm drw.o &
+rm dwm &
+rm dwm.o &
+rm util.o &
+
+while getopts 'imu' OPTION; do
   case "$OPTION" in
+    i)
+      echo "Make and install"
+      make && sudo make install
+      ;;
     m)
-      echo "Make, without install"
+      echo "Make only"
+      make
       ;;
     u)
-      echo "Uninstalling"
+      echo "Uninstall"
+      sudo make uninstall
       ;;
     ?)
-      echo "Script usage: $(basename \$0) -m - make, not install, -u uninstall" >&2
+      echo "Script usage: $(basename \$0) -i make+install -m - make, -u uninstall" >&2
       exit 1
       ;;
   esac
 done
 shift "$(($OPTIND -1))"
 
-echo "Ended"
+echo "Cleaning"
+rm config.h &
+rm drw.o &
+rm dwm &
+rm dwm.o &
+rm util.o &
+
+echo "Finished"
